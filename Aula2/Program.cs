1833301fd7ac9bf4a09b2914433ca23ecff147c4
curso.ads <curso.ads@LAB06-03>
2022-08-29 20:45:04 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto prod = new Produto();
+
+
+            Console.Write("Digite o código do produto: ");
+            prod.setCodigo(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Digite a descrição do produto: ");
+            prod.setDescricao(Console.ReadLine());
+
+            Console.Write("Digite o valor do produto: ");
+            prod.setValor(Convert.ToDouble(Console.ReadLine()));
+
+            Console.Write("Digite a categoria do produto: ");
+            prod.setCategoria(Console.ReadLine());
+
+
+
+            Console.WriteLine(prod.getCodigo());
+            Console.WriteLine(prod.getValor());
+            Console.WriteLine(prod.getDescricao());
+            Console.WriteLine(prod.getCategoria());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
